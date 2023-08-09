711f5706bdeedd80d13db6425fbdb1449b425a6c
Riad <119328612+riadghub@users.noreply.github.com>
2023-08-09 19:26:43 +0200

    Ajout des versions mâle/femelle d'un pokemon

--- src/components/PokemonCollection.tsx
+++ src/components/PokemonCollection.tsx
@@ -7,16 +7,16 @@ interface Props {
   pokemons: Pokemon[];
 }
 
-// ... import statements ...
-
 const PokemonColection: React.FC<Props> = (props) => {
   const { pokemons } = props;
   const [selectedPokemon, setSelectedPokemon] = useState<Pokemon | null>(null);
   const [showShiny, setShowShiny] = useState(false);
+  const [selectedGender, setSelectedGender] = useState<"male" | "female" | null>(null);
 
   const handlePokemonClick = (pokemon: Pokemon) => {
     setSelectedPokemon(pokemon);
     setShowShiny(false);
+    setSelectedGender(null);
   };
 
   const handleCloseDetails = (event: React.MouseEvent) => {
@@ -27,6 +27,10 @@ const PokemonColection: React.FC<Props> = (props) => {
 
   const handleToggleShiny = () => {
     setShowShiny(!showShiny);
+  };
+
+  const handleGenderSelect = (gender: "male" | "female") => {
+    setSelectedGender(gender);
   };
 
   return (
@@ -43,7 +47,15 @@ const PokemonColection: React.FC<Props> = (props) => {
                 key={pokemon.id}
                 name={pokemon.name}
                 id={pokemon.id}
-                image={showShiny && selectedPokemon === pokemon ? pokemon.sprites.front_shiny : pokemon.sprites.front_default}
+                image={
+                  showShiny && selectedPokemon === pokemon
+                    ? selectedGender === "female"
+                      ? pokemon.sprites.front_shiny_female || pokemon.sprites.front_shiny
+                      : pokemon.sprites.front_shiny
+                    : selectedGender === "female"
+                    ? pokemon.sprites.front_female || pokemon.sprites.front_default
+                    : pokemon.sprites.front_default
+                }
                 type={pokemon.types[0].type.name}
               />
             </div>
@@ -53,11 +65,40 @@ const PokemonColection: React.FC<Props> = (props) => {
       {selectedPokemon && (
         <div className={`overlay`} onClick={handleCloseDetails}>
           <div className={`selected-pokemon-details ${selectedPokemon.types[0].type.name}`}>
-            <img src={showShiny ? selectedPokemon.sprites.front_shiny : selectedPokemon.sprites.front_default} alt={selectedPokemon.name} />
+            <img
+              src={
+                showShiny
+                  ? selectedGender === "female"
+                    ? selectedPokemon.sprites.front_shiny_female || selectedPokemon.sprites.front_shiny
+                    : selectedPokemon.sprites.front_shiny
+                  : selectedGender === "female"
+                  ? selectedPokemon.sprites.front_female || selectedPokemon.sprites.front_default
+                  : selectedPokemon.sprites.front_default
+              }
+              alt={selectedPokemon.name}
+            />
             <h2>ID: {selectedPokemon.id}</h2>
             <h2>{selectedPokemon.name}</h2>
             <p>Type: {selectedPokemon.types[0].type.name}</p>
-            <button onClick={handleToggleShiny}>Version Shiny</button>
+            {selectedPokemon.sprites.front_female !== null ? (
+              <div>
+                <p>Gender: {selectedGender || "Unknown"}</p>
+                <button onClick={handleToggleShiny}>Version Shiny</button>
+                <div>
+                  <button onClick={() => handleGenderSelect("male")}>Male</button>
+                  <button onClick={() => handleGenderSelect("female")}>Female</button>
+                </div>
+              </div>
+            ) : (
+              <div>
+                <p>
+                  Il n'y a pas de différence <br></br>
+                  entre la version mâle <br></br>
+                  et femelle.
+                </p>
+                <button onClick={handleToggleShiny}>Version Shiny</button>
+              </div>
+            )}
           </div>
         </div>
       )}
--- src/interface.ts
+++ src/interface.ts
@@ -3,7 +3,9 @@ export interface Pokemon {
     name: string;
     sprites: {
       front_default: string;
+      front_female: string;
       front_shiny: string;
+      front_shiny_female: string;
     };
     types: {
       type : {
